0bb03a63a137cfb3082b08df43f701802d41f3f0
Michael de Silva <michael@mwdesilva.com>
2020-05-20 15:23:45 +0530

    Use trait object to allow values of different types in vector

--- src/monitors/mod.rs
+++ src/monitors/mod.rs
@@ -1,56 +1,196 @@
-use super::errors::Error;
-use crate::configure;
-use crate::log::LevelFilter;
-use crate::log4rs::append::file::FileAppender;
-use crate::log4rs::config::{Appender, Config, Root};
-use crate::log4rs::encode::pattern::PatternEncoder;
-use crate::utils;
-use crate::wrappers;
-use ::chrono::Utc;
+use std::clone;
 use std::collections::VecDeque;
+use std::default::Default;
 use std::error;
+use std::fmt;
+use std::marker::Copy;
 
-use std::time::Duration;
-use std::{thread, time};
-
-#[derive(Default)]
-pub struct Monitors {
-    pub enabled: Vec<Monitor>,
-}
-
-#[derive(Copy, Clone)]
-enum MonitorState {
-    Good,
-    Failed,
+#[derive(Debug, Copy, Clone)]
+pub enum State {
+    Up,
+    Down,
     Unknown,
 }
 
 #[derive(Copy, Clone)]
-pub struct Monitor {
-    state: MonitorState,
+pub enum Alerts {
+    PollFailure,
+    StateChange,
+    UnknownFailure,
 }
 
-impl Monitors {
-    pub async fn new() -> Result<Monitors, Box<dyn error::Error>> {
-        Ok(Monitors::default())
+#[derive(Debug, Copy, Clone)]
+pub struct PollHTTPBodyContent;
+
+#[derive(Debug, Copy, Clone)]
+pub struct PollHTTPStatusOk;
+
+#[derive(Debug, Copy, Clone)]
+pub struct PollMySQLDBSlave;
+
+pub trait Monitorable {
+    fn info(&self) -> String;
+    fn poll(&self);
+}
+
+pub struct Monitor<T> {
+    context: T,
+    state: State,
+}
+
+pub struct Monitored {
+    pub enabled: Vec<Box<dyn Monitorable>>,
+}
+
+impl Default for State {
+    fn default() -> Self {
+        State::Unknown
+    }
+}
+
+impl Default for Monitored {
+    fn default() -> Monitored {
+        Monitored {
+            enabled: Vec::new(),
+        }
+    }
+}
+
+impl fmt::Debug for Monitored {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        f.debug_list().entries(&self.enabled).finish()
+    }
+}
+
+impl fmt::Debug for dyn Monitorable {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        f.debug_struct("Monitorable")
+            .field("info", &self.info())
+            .finish()
+    }
+}
+
+impl Monitored {
+    pub async fn new() -> Result<Monitored, Box<dyn error::Error>> {
+        Ok(Monitored::default())
     }
 
-    pub async fn add(&mut self, monitor: &Monitor) -> Result<(), Box<dyn error::Error>> {
-        self.enabled.push(*monitor);
+    pub async fn add<T: 'static + Monitorable>(
+        &mut self,
+        item: Box<T>,
+    ) -> Result<(), Box<dyn error::Error>> {
+        self.enabled.push(item);
 
         Ok(())
     }
 }
 
+// PollHTTPBodyContent
+impl fmt::Debug for Monitor<PollHTTPBodyContent> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        f.debug_struct("Monitor<PollHTTPBodyContent>")
+            .field("state", &self.state)
+            .field("context", &self.context)
+            .finish()
+    }
+}
+
+impl Default for Monitor<PollHTTPBodyContent>
+where
+    State: Default,
+{
+    fn default() -> Monitor<PollHTTPBodyContent> {
+        Monitor::<PollHTTPBodyContent> {
+            context: PollHTTPBodyContent {},
+            state: State::default(),
+        }
+    }
+}
+
+impl Copy for Monitor<PollHTTPBodyContent> {}
+
+impl clone::Clone for Monitor<PollHTTPBodyContent> {
+    fn clone(&self) -> Self {
+        *self
+    }
+}
+
+impl Monitorable for Monitor<PollHTTPBodyContent> {
+    fn info(&self) -> String {
+        String::from("Monitor<PollHTTPBodyContent>")
+    }
+    fn poll(&self) {
+        println!("poll() for {:#?}", self);
+    }
+}
+
+impl Monitor<PollHTTPBodyContent> {
+    pub async fn new() -> Result<Monitor<PollHTTPBodyContent>, Box<dyn error::Error>> {
+        let monitor: Monitor<PollHTTPBodyContent> = Monitor::<PollHTTPBodyContent>::default();
+        Ok(monitor)
+    }
+}
+
+// PollHTTPStatusOk
+impl fmt::Debug for Monitor<PollHTTPStatusOk> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        f.debug_struct("Monitor<PollHTTPStatusOk>")
+            .field("state", &self.state)
+            .field("context", &self.context)
+            .finish()
+    }
+}
+
+impl Default for Monitor<PollHTTPStatusOk>
+where
+    State: Default,
+{
+    fn default() -> Monitor<PollHTTPStatusOk> {
+        Monitor::<PollHTTPStatusOk> {
+            context: PollHTTPStatusOk {},
+            state: State::default(),
+        }
+    }
+}
+
+impl Copy for Monitor<PollHTTPStatusOk> {}
+
+impl clone::Clone for Monitor<PollHTTPStatusOk> {
+    fn clone(&self) -> Self {
+        *self
+    }
+}
+
+impl Monitorable for Monitor<PollHTTPStatusOk> {
+    fn info(&self) -> String {
+        String::from("Monitor<PollHTTPStatusOk>")
+    }
+    fn poll(&self) {
+        println!("poll() for {:#?}", self);
+    }
+}
+
+impl Monitor<PollHTTPStatusOk> {
+    pub async fn new() -> Result<Monitor<PollHTTPStatusOk>, Box<dyn error::Error>> {
+        let monitor: Monitor<PollHTTPStatusOk> = Monitor::<PollHTTPStatusOk>::default();
+        Ok(monitor)
+    }
+}
+
 pub async fn run() -> Result<(), Box<dyn error::Error>> {
-    let mut monitors = Monitors::new().await?;
-    let new_monitor = Monitor {
-        state: MonitorState::Unknown,
-    };
-    monitors.add(&new_monitor).await?;
+    let mut monitored = Monitored::new().await?;
+    let new_monitor: Monitor<PollHTTPBodyContent> = Monitor::<PollHTTPBodyContent>::new().await?;
+    let new_monitor2: Monitor<PollHTTPStatusOk> = Monitor::<PollHTTPStatusOk>::new().await?;
+    monitored.add(Box::new(new_monitor)).await?;
+    monitored.add(Box::new(new_monitor2)).await?;
 
-    println!("Enabled monitor count: {}", monitors.enabled.len());
+    println!("Monitored: {:#?}", &monitored);
+    println!("Enabled monitor count: {}", &monitored.enabled.len());
 
-    info!("monitors run");
+    for item in monitored.enabled.iter() {
+        item.poll();
+    }
+
+    info!("monitors::run()");
     Ok(())
 }
